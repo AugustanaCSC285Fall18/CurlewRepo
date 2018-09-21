8b157b6e769722a4f09b4fdc0d2c503c27315ac7
brysonadcock16 <brysonadcock16@OLINLAB204-19A.augustana.edu>
2018-09-21 14:21:53 -0500

    Added TimePoint Class

---
+++ ChickTracker/src/TimePoint.java
@@ -0,0 +1,25 @@
+import java.awt.Point;
+
+public class TimePoint {
+	// data fields
+	private int frameNum;
+	private Point pt;
+	
+	
+	public int getFrameNum() {
+		return frameNum;
+	}
+	
+	public void setFrameNum(int frameNum) {
+		this.frameNum = frameNum;
+	}
+	
+	public Point getPt() {
+		return pt;
+	}
+	
+	public void setPt(Point pt) {
+		this.pt = pt;
+	}
+	
+}
