07555259b8552f0db5179c4bdeee51c4d89402c9
rileysmith17 <rileysmith17@OLINLAB204-04A.augustana.edu>
2018-09-24 14:15:31 -0500

    added grid class

---
+++ ChickTracker/src/datamodel/Grid.java
@@ -0,0 +1,81 @@
+package datamodel;
+
+public class Grid {
+	
+	private double pixelColumnWidth;
+	private double pixelRowWidth;
+	private double gridHeight;
+	private double gridWidth;
+
+	public Grid(double xPixelsPerCm, double yPixelsPerCm, int numRows, int numColumns) {
+		setPixelColumnWidth((gridWidth/numColumns) * xPixelsPerCm);
+		setPixelRowWidth((gridHeight/numRows) * yPixelsPerCm);
+	}
+
+	
+	
+	
+	
+	
+	public double getPixelColumnWidth() {
+		return pixelColumnWidth;
+	}
+
+	public void setPixelColumnWidth(double pixelColumnWidth) {
+		this.pixelColumnWidth = pixelColumnWidth;
+	}
+
+	public double getPixelRowWidth() {
+		return pixelRowWidth;
+	}
+
+	public void setPixelRowWidth(double pixelRowWidth) {
+		this.pixelRowWidth = pixelRowWidth;
+	}
+
+
+
+
+
+
+	public double getGridHeight() {
+		return gridHeight;
+	}
+
+
+
+
+
+
+	public void setGridHeight(double gridHeight) {
+		this.gridHeight = gridHeight;
+	}
+
+
+
+
+
+
+	public double getGridWidth() {
+		return gridWidth;
+	}
+
+
+
+
+
+
+	public void setGridWidth(double gridWidth) {
+		this.gridWidth = gridWidth;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
