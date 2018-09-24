5e1b805f994e31b053c269a0c6c734f9f79ffef7
bryson <brysonadcock16@augustana.edu>
2018-09-24 13:27:27 -0500

    Moved TimePoint class to datamodel package

--- ChickTracker/src/datamodel/TimePoint.java
+++ ChickTracker/src/datamodel/TimePoint.java
@@ -1,25 +1,22 @@
+package datamodel;
 import java.awt.Point;
+
 
 public class TimePoint {
 	// data fields
 	private int frameNum;
 	private Point pt;
 	
+	public TimePoint() {
+		
+	}
 	
 	public int getFrameNum() {
 		return frameNum;
 	}
 	
-	public void setFrameNum(int frameNum) {
-		this.frameNum = frameNum;
-	}
-	
 	public Point getPt() {
 		return pt;
-	}
-	
-	public void setPt(Point pt) {
-		this.pt = pt;
 	}
 	
 }
