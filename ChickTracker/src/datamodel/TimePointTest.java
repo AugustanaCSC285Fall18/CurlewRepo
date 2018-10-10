79bb83c9578771faa421185d73b9a3fad7929f0f
Riley Smith <rileysmith17@augustana.edu>
2018-10-10 13:48:53 -0500

    flushed out the TimePointTestClass

--- ChickTracker/src/datamodel/TimePointTest.java
+++ ChickTracker/src/datamodel/TimePointTest.java
@@ -10,14 +10,35 @@ import org.junit.jupiter.api.Test;
 class TimePointTest {
 
 	@Test
-	void testPoints() {
+	void testPointFrameNum() {
 		List<TimePoint> test = new ArrayList<TimePoint>();
-		test.add(new TimePoint(100,100,0));		
-		test.add(new TimePoint(110,110,1));		
-		test.add(new TimePoint(150,200,5));		
+		test.add(new TimePoint(100,100,0));
+		test.add(new TimePoint(110,110,1));
+		test.add(new TimePoint(150,200,5));
 		assertEquals(0,test.get(0).getFrameNum());
 		assertEquals(1,test.get(1).getFrameNum());
 		assertEquals(5,test.get(2).getFrameNum());
+	}
+	
+	@Test
+	void testPointxCoordinates() {
+		List<TimePoint> test = new ArrayList<TimePoint>();
+		test.add(new TimePoint(100,100,0));
+		test.add(new TimePoint(-110,110,1));
+		test.add(new TimePoint(0,200,5));
+		assertEquals(100,test.get(0).getX());
+		assertEquals(-110,test.get(1).getX());
+		assertEquals(0,test.get(2).getX());
+	}
+	
+	void testPointYCoordinates() {
+		List<TimePoint> test = new ArrayList<TimePoint>();
+		test.add(new TimePoint(100,100,0));
+		test.add(new TimePoint(-110,-110,1));
+		test.add(new TimePoint(0,0,5));
+		assertEquals(100,test.get(0).getY());
+		assertEquals(-110,test.get(1).getY());
+		assertEquals(0,test.get(2).getY());
 	}
 
 }
