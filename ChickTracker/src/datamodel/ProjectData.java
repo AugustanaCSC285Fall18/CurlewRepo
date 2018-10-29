5e0d1e940320f7df98e54668689212e661c64741
bryson <brysonadcock16@augustana.edu>
2018-10-28 23:09:17 -0500

    Removed unused imports and data fields to remove warnings

--- ChickTracker/src/datamodel/ProjectData.java
+++ ChickTracker/src/datamodel/ProjectData.java
@@ -94,6 +94,7 @@ public class ProjectData {
 	}
 	
 	public static ProjectData loadFromFile(File loadFile) throws FileNotFoundException {
+		@SuppressWarnings("resource")
 		String json = new Scanner(loadFile).useDelimiter("\\Z").next();
 		return fromJSON(json);
 	}
